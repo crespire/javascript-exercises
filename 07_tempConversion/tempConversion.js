4c498cabd1e63fc41b47a45473a321b5d9349ce1
crespire <li.simmon@gmail.com>
2021-09-30 11:46:36 -0400

    Completed. Remember to check the type of the return to ensure you are working with the right stuff.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let result = ((temp - 32) * 5/9).toFixed(1);
+  return Number(result);
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let result = (temp * 9/5 + 32).toFixed(1);
+  return Number(result);
 };
 
 // Do not edit below this line
