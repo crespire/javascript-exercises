83fda92e25bc757157cf9f422e193f0819660bed
crespire <li.simmon@gmail.com>
2021-09-30 11:01:18 -0400

    Remember that arr.forEach method provides a way to iterate over an array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...filteredArgs) {
+    const filterArray = Array.from(filteredArgs);
+    let resultArr = new Array();
 
+    arr.forEach(function(element) {
+        if (!filterArray.includes(element)) {
+            return resultArr.push(element);
+        }
+    });
+
+    return resultArr;
 };
 
 // Do not edit below this line
