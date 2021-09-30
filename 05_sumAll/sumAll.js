901682774ae7f496c920bd784c77d5c4db695ec9
crespire <li.simmon@gmail.com>
2021-09-30 11:16:20 -0400

    Completed exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(firstInput, secondInput) {
+    let result = 0;
 
+    if (typeof(firstInput) !== typeof(0) || typeof(secondInput) !== typeof(0)) {
+        return 'ERROR';
+    } else if (firstInput < 0 || secondInput < 0) {
+        return 'ERROR';
+    }
+
+    let start = (firstInput < secondInput) ? firstInput : secondInput;
+    let end = (firstInput < secondInput) ? secondInput : firstInput;
+    let i = start;
+
+    do {
+        result += i;
+        i++;
+    } while (i<=end)
+    
+    return result;
 };
 
 // Do not edit below this line
