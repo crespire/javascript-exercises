56cd99b38d82126d7e0f10991253a81407b0f530
crespire <li.simmon@gmail.com>
2021-10-05 12:39:11 -0400

    Finished with regex

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let arrWord = Array.from(word).join();
+    let revWord = Array.from(word).reverse().join();
 
+    let regex = /[\W_]/g;
+    strippedWord = arrWord.toLowerCase().replace(regex, '');
+    strippedRev = revWord.toLowerCase().replace(regex, '');
+
+    return strippedWord === strippedRev;
 };
 
 // Do not edit below this line
