dd59b7212b0c61158a9846c163422f934d7248cf
crespire <li.simmon@gmail.com>
2021-10-06 20:01:42 -0400

    Finishes the function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    if (number < 0) return "OOPS"
+    arr = [0, 1];
+    for (let i = 1; i < +number; i++) {
+        arr.push(arr[i] + arr[i-1]);
+    }
+    return arr[+number];
 };
 
 // Do not edit below this line
